c3ba30bb0ea2ac53de8bae95b9b4928d236360c1
Nick Roshdieh <nickroshd@gmail.com>
2022-03-16 01:36:04 -0400

    added db functions and error handling

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
-from backend import database
+from backend import database, model
 
-database.fetch_one_todo()
 
 app = FastAPI()
 
@@ -24,24 +23,53 @@ def read_root():
 
 @app.get("/api/todo")
 async def get_todo():
-    return 1
+    response = await database.fetch_all_todos()
+    return response
 
 
-@app.get("/api/todo/{id}")
-async def get_todo_by_id(id):
-    return 1
+@app.get("/api/todo/{title}", response_model=model.Todo)
+async def get_todo_by_id(title):
+    response = await database.fetch_one_todo(title)
+    if response:
+        return response
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'there is no item with title: {title}'
+        )
 
 
-@app.post("/api/todo")
-async def post_todo():
-    return 1
+@app.post("/api/todo", response_model=model.Todo)
+async def post_todo(todo: model.Todo):
+    response = await database.create_todo(todo.dict())
+    if response:
+        return response
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Something went wrong, bad request'
+        )
 
 
-@app.put("/api/todo/{id}")
-async def update_todo(id):
-    return 1
+@app.put("/api/todo/{title}", response_model=model.Todo)
+async def put_todo(title: str, description: str):
+    response = await database.update_todo(title, description)
+    if response:
+        return response
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'there is no item with title: {title}'
+        )
 
 
-@app.delete("/api/todo/{id}")
-async def delete_todo(id):
-    return 1
+@app.delete("/api/todo/{title}")
+async def delete_todo(title):
+    response = await database.delete_todo(title)
+    if response:
+        return "Success deleted Todo Item"
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'there is no item with title: {title}'
+        )
